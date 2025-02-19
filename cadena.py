9a41308ba06585070407c56fc8b1fa921656ca23
mariana <cor.mavalosc24@alumnos.cecyteg.edu.mx>
2025-02-19 10:39:09 -0600

    actualizacion en cadena y ejemplo

--- cadena.py
+++ cadena.py
@@ -1,1 +1,2 @@
-print(" las cadenaS O strings en python de delimitan con comillas dobles o simple")
+print(" las cadenas o strings en python de delimitan con comillas dobles o simples")
+print (" esto es una cadena", 5*9)
